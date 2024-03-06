ec04650de8a2c21f80013043e125a16d9d826c1a
technical09 <technical09@analisis.net>
2024-03-06 19:07:03 +0100

    Funcionalidad login

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,25 +4,51 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema=z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+      invalid_type_error: 'Por favor, selecciona un cliente.',
+    }),
+    amount: z.coerce
+      .number()
+      .gt(0, {message: 'Por favor, introduce una cantidad mayor de $0'}),
+    status: z.enum(['pending', 'paid'],{
+      invalid_type_error: 'Por favor, selecciona un estado de la factura.',
+    }),
     date: z.string(),
 });
 
 const CreateInvoice=FormSchema.omit({id: true, date: true});
 const UpdateInvoice=FormSchema.omit({id: true, date:true});
 
-export async function createInvoice(formData: FormData) {
-  const {customerId, amount, status} = CreateInvoice.parse({
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?:string|null;
+}
+
+
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
       status: formData.get('status'),
   });
-  const aumountInCents = amount*100;
+
+  if(!validatedFields.success){
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Faltan campos. Fallo al crear la factura.',
+    }
+  }
+  const { customerId, amount, status } = validatedFields.data;
+  const aumountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
   try{
@@ -73,5 +99,24 @@ export async function deleteInvoice(id: string){
     return {
       message: 'Error BD: No se pudo elimiar la Factura.'
     };
+  }
+}
+
+export async function authenticate(
+  prevState: string|undefined,
+  formData: FormData,
+){
+  try{
+    await signIn('credentials', formData);
+  }catch(error){
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Credenciales erróneas.';
+        default:
+          return 'Ha ocurrido algún error.';
+      }
+    }
+    throw error;
   }
 }
