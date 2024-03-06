80662df5b9d27e5ace5a5a4ea56e9c0dd01f5354
technical09 <technical09@analisis.net>
2024-03-06 13:50:59 +0100

    Funcional previo a errors.tsx

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	experimental: {
+		serverActions: {
+			allowedOrigins: ["localhost:3000", "127.0.0.1:3000"],
+		},
+	},
+	
+};
 
 module.exports = nextConfig;
